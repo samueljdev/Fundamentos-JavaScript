7954b7e88614136d7c469850a815d9866a6f31a7
samuel <samuel.jdev11@gmail.com>
2026-01-29 17:19:23 -0400

    Operadores aritméticos

--- 1-Operadores/js/scripts.js
+++ 1-Operadores/js/scripts.js
@@ -3,3 +3,10 @@ console.log("Number == ");
 console.log(typeof 2);
 console.log(typeof 2.2);
 console.log(typeof -4);
+
+// 2 - Operadores aritméticos
+console.log("Operadores aritméticos ==");
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
