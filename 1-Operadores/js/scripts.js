4cb0909644f6080b26a3e383b7e2f81c9e0c694b
samuel <samuel.jdev11@gmail.com>
2026-01-29 17:23:15 -0400

    Special Numbers

--- 1-Operadores/js/scripts.js
+++ 1-Operadores/js/scripts.js
@@ -10,3 +10,9 @@ console.log(2 + 4);
 console.log(10 - 5);
 console.log(5 * 4);
 console.log(10 / 2);
+
+// 3 - Special Numbers
+console.log("Special Numbers ==");
+console.log(typeof Infinity);
+console.log(12 * "asd");
+console.log(typeof NaN);
