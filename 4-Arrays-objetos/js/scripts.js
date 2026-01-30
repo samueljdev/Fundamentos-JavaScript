f179877a9b7191103a1448b8408486634420356e
samuel <samuel.jdev11@gmail.com>
2026-01-30 17:00:18 -0400

    Propriedades

--- 4-Arrays-objetos/js/scripts.js
+++ 4-Arrays-objetos/js/scripts.js
@@ -15,3 +15,13 @@ const arr = ["a", "b", "c", "d"];
 console.log(arr[0]);
 console.log(arr[2]);
 console.log(arr[10]);
+
+// 3 - Propriedades
+console.log(" ==== Propriedades ====");
+
+const numbers = [5, 12, 4, 22];
+console.log(numbers.length);
+console.log(numbers["length"]);
+
+const myName = "Matheus";
+console.log(myName.length);
