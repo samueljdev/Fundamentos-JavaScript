bc7742ad850425afb62726f575a905e682c5c2f8
samuel <samuel.jdev11@gmail.com>
2026-01-31 11:08:01 -0400

    Indexof e lastindexof

--- 4-Arrays-objetos/js/scripts.js
+++ 4-Arrays-objetos/js/scripts.js
@@ -158,3 +158,21 @@ letters.unshift("z", "x", "y");
 
 letters.unshift("p");
 console.log(letters);
+
+// 13 - Indexof e lastindexof
+console.log(" ==== Indexof e lastindexof ====");
+
+const myElements = ["Morango", "Maçã", "Abacate", "Pêra", "Abacate"];
+
+console.log(myElements.indexOf("Maçã"));
+console.log(myElements.indexOf("Abacate"));
+
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Abacate"));
+
+console.log(myElements.lastIndexOf("Maçã"));
+
+console.log(myElements.indexOf("Mamão"));
+console.log(myElements.lastIndexOf("Mamão"));
