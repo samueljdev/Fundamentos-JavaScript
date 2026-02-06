011310e724acf40cff40c5335ecde783d840986c
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:03:33 -0400

    Variáveis

---
+++ 2-Estruturas/js/scripts.js
@@ -0,0 +1,11 @@
+// 1 - Variáveis
+console.log("Variáveis ==");
+
+let nome = "Matheus";
+console.log(nome);
+
+nome = "Matheus Battisti";
+console.log(nome);
+
+const idade = 31;
+console.log(idade);
