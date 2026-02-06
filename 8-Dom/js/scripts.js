a01eaebd4f3cc434cd95d35f27169952f6462ddd
samuel <samuel.jdev11@gmail.com>
2026-02-06 17:02:33 -0400

    Movendo-se no DOM

---
+++ 8-Dom/js/scripts.js
@@ -0,0 +1,14 @@
+// 1 -Movendo-se no DOM
+console.log(" ==== Movendo-se no DOM ====");
+
+console.log("Movendo-se no DOM == ");
+
+console.log(document.body);
+
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].textContent);
+console.log(document.body.childNodes[1].textContent.length);
