c70d59b1553745e5d9da283412c102d5ae97609a
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:24:27 -0400

    Destructuring me arrays

--- 4-Arrays-objetos/js/scripts.js
+++ 4-Arrays-objetos/js/scripts.js
@@ -334,3 +334,11 @@ console.log(firstName, lastName, job);
 const { firstName: primeiroNome } = userDetails;
 
 console.log(firstName);
+
+// 26 - Destructuring me arrays
+console.log(" ==== Destructuring arrays ====");
+
+const myList = ["Avião", "Submarino", "Carro"];
+
+const [veiculoA, veiculoB, veiculoC] = myList;
+console.log(veiculoA, veiculoB, veiculoC);
