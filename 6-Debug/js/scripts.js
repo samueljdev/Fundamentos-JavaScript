bf799323ab53b3eba0c7f52defd07fd94e6914e3
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:35:45 -0400

    Assertion

--- 6-Debug/js/scripts.js
+++ 6-Debug/js/scripts.js
@@ -110,3 +110,18 @@ try {
 } finally {
     console.log("O Código foi executado!");
 }
+
+// 8 - Assertion
+console.log(" ==== Assertion ====");
+
+function checkArray(arr) {
+    if (arr.length === 0) {
+        throw new Error("O array precisa ter elementos!");
+    } else {
+        console.log(`O array tem ${arr.length} elementos!`);
+    }
+}
+
+// checkArray([]);
+
+checkArray([1, 2, 3]);
