d75c4afbdcb133df8448145f28a86cba771c06ea
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:17:35 -0400

    Arrays

---
+++ 4-Arrays-objetos/js/scripts.js
@@ -0,0 +1,9 @@
+// 1 - Arrays
+console.log(" ==== Arrays ====");
+
+const lista = [1, 2, 3, 4, 5];
+console.log(lista);
+console.log(typeof lista);
+
+const itens = ["Matheus", true, 2, 4.12];
+console.log(itens);
