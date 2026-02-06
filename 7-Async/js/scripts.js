0d95096b8e21f9a82ae6af753af76cd85eb0d0fe
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:40:25 -0400

    Rejeição

--- 7-Async/js/scripts.js
+++ 7-Async/js/scripts.js
@@ -45,3 +45,27 @@ Promise.resolve(4 * "asd")
         }
     })
     .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 5 - Rejeição
+console.log(" ==== Rejeição ====");
+
+function checkNumber(n) {
+    return new Promise((resolve, reject) => {
+        if (n > 10) {
+            resolve(`O número é maior que 10!`);
+        } else {
+            reject(new Error("Número muito baixo!"));
+        }
+    });
+}
+
+const a = checkNumber(20);
+const b = checkNumber(1);
+
+a.then((v) => console.log(`O resultado é: ${v}`)).catch((err) =>
+    console.log(`Um erro ocorreu: ${err}`)
+);
+
+b.then((v) => console.log(`O resultado é: ${v}`)).catch((err) =>
+    console.log(`Um erro ocorreu: ${err}`)
+);
