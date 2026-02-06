13cca0c13a7757ebf3943513e0275b44af8ce4bb
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:26:29 -0400

    Aprofundando em métodos

--- 5-Orientacao-objetos/js/scripts.js
+++ 5-Orientacao-objetos/js/scripts.js
@@ -9,3 +9,24 @@ const animal = {
 };
 
 animal.latir();
+
+// 2 - Aprofundando em métodos
+console.log(" ==== Aprofundando em métodos ====");
+
+const pessoa = {
+    nome: "Henrique",
+
+    getNome: function () {
+        return this.nome;
+    },
+
+    setNome: function (novoNome) {
+        this.nome = novoNome;
+    },
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.getNome());
+
+pessoa.setNome("Henrique Battisti");
+console.log(pessoa.getNome());
