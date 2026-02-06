63c35b3e2891462bc4607eaa03769b0e30c13c23
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:10:08 -0400

    Escopo da função

--- 3-Funcoes/js/scripts.js
+++ 3-Funcoes/js/scripts.js
@@ -32,3 +32,15 @@ const resultado = soma(a, b);
 console.log(resultado);
 
 console.log(soma(c, d));
+
+// 3 - Escopo da função
+console.log(" ==== Escopo da função ====");
+
+let y = 10;
+function testandoEscopo() {
+    let y = 20;
+    console.log(`Y dentro da função: ${y}`);
+}
+console.log(`Y fora da função: ${y}`);
+
+testandoEscopo();
