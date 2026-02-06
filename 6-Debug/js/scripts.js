f9f782b2c74f7d25c6ebc63727cede605c25654f
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:33:56 -0400

    Console.log

--- 6-Debug/js/scripts.js
+++ 6-Debug/js/scripts.js
@@ -10,3 +10,26 @@ const opa = "agora sim!";
 // const undefined = 10;
 
 // delete [].length;
+
+// 2 - Console.log
+console.log(" ==== Console.log ====");
+
+let a = 1;
+let b = 2;
+
+if (a == 1) {
+    a = b + 2;
+}
+
+console.log(a, b);
+
+for (let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
+
+if (a > 5) {
+    a = 25;
+}
+
+console.log(a);
