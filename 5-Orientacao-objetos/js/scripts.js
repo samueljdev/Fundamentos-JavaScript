4f127884244c5f73859944a267af0ceca5b0b5a1
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:32:49 -0400

    Instanceof

--- 5-Orientacao-objetos/js/scripts.js
+++ 5-Orientacao-objetos/js/scripts.js
@@ -239,3 +239,14 @@ class Lobo extends Mamifero {
 
 const shark = new Lobo(4, "Shark");
 console.log(shark);
+
+// 15 - Instanceof
+console.log(" ==== Instanceof ====");
+
+console.log(shark instanceof Lobo);
+
+console.log(Lobo instanceof Mamifero);
+
+console.log(new Lobo(4, "teste") instanceof Mamifero);
+
+console.log(new Post("a", "b") instanceof Cachorro);
