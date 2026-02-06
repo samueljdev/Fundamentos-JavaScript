21e4e99fe26d78b0c26c2aee969ac70b7b10abff
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:15:53 -0400

    Mais sobre objetos

--- 4-Arrays-objetos/js/scripts.js
+++ 4-Arrays-objetos/js/scripts.js
@@ -69,3 +69,20 @@ console.log(car);
 
 delete car.km;
 console.log(car);
+
+// 7 - Mais sobre objetos
+console.log(" ==== Mais sobre objetos ====");
+
+const obj = {
+    a: "teste",
+    b: true,
+};
+
+console.log(obj instanceof Object);
+
+const obj2 = {
+    c: [],
+};
+
+Object.assign(obj2, obj);
+console.log(obj2);
