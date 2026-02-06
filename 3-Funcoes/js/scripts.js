46453a58f9de8d430d1cbe99102a1f1e1ce7b976
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:16:31 -0400

    Recursion

--- 3-Funcoes/js/scripts.js
+++ 3-Funcoes/js/scripts.js
@@ -176,3 +176,39 @@ console.log(c2);
 
 console.log(c1(5));
 console.log(c2(10));
+
+// 11 - Recursion
+console.log(" ==== Recursion ====");
+
+const untilTen = (n, m) => {
+    if (n < 10) {
+        console.log("A função parou de executar!");
+    } else {
+        const x = n - m;
+        console.log(x);
+        untilTen(x, m);
+    };
+};
+
+untilTen(100, 7);
+
+// infinite recursion, tomar cuidado
+function run() {
+    console.log("Executando...");
+    run();
+}
+
+// run();
+
+function factorial(x) {
+    if (x === 0) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    };
+}
+
+const num = 6;
+const result = factorial(num);
+
+console.log(`O fatorial do número ${num} é ${result}`);
