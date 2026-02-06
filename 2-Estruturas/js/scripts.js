58559b7b1ccbb76d87296187caeaa04f342009d5
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:08:28 -0400

    Switch

--- 2-Estruturas/js/scripts.js
+++ 2-Estruturas/js/scripts.js
@@ -188,3 +188,36 @@ for (s = 0; s < 10; s++) {
     }
     console.log(s);
 }
+
+// 16 - Switch
+console.log("Switch ==");
+
+const job = "Advogado";
+switch (job) {
+    case "Programador":
+        console.log("Voce é um programador!");
+        break;
+    case "Advogado":
+        console.log("Voce é um advogado!");
+        break;
+    case "Engenheiro":
+        console.log("Voce é um engenheiro!");
+        break;
+    default:
+        console.log("Profissão não encontrada");
+}
+
+// switch "errado"
+const l = 100;
+switch (l) {
+    case 200:
+        console.log("L é 200!");
+    case 100:
+        console.log("L é 100!");
+    case 10:
+        console.log("L é 10!");
+    case 110:
+        console.log("L é 110!");
+    default:
+        console.log("L não foi encontrado");
+}
