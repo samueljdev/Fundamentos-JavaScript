fc5fd12b8310a54bc511e43a41e15965c4391bf0
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:38:52 -0400

    SetTimeout

---
+++ 7-Async/js/scripts.js
@@ -0,0 +1,10 @@
+// 1 - SetTimeout
+console.log(" ==== SetTimeout ====");
+
+console.log("Ainda não executou");
+
+setTimeout(function () {
+    console.log("Requisição assíncrona!");
+}, 2000);
+
+console.log("Ainda não executou 2");
