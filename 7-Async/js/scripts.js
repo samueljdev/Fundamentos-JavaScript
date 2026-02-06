2cac5729c44f6dafa8ecbf59f363ad7de8b0abfd
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:42:02 -0400

    Await

--- 7-Async/js/scripts.js
+++ 7-Async/js/scripts.js
@@ -101,3 +101,22 @@ async function somarComDelay(a, b) {
 somarComDelay(2, 4).then((value) => {
     console.log(`O valor da soma é: ${value}`);
 });
+
+// 8 - Await
+console.log(" ==== Await ====");
+
+function resolveComDelay() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Resolveu Promise");
+        }, 2000);
+    });
+}
+
+async function chamadaAsync() {
+    console.log("Chamando Promise, e esperando resultado...");
+    const result = await resolveComDelay();
+    console.log(`O resultado chegou: ${result}`);
+}
+
+chamadaAsync();
