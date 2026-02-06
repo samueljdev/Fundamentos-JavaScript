e735a26933579e41a5bfb498607444fee157c267
samuel <samuel.jdev11@gmail.com>
2026-02-06 10:39:32 -0400

    Promise

--- 7-Async/js/scripts.js
+++ 7-Async/js/scripts.js
@@ -19,3 +19,18 @@ console.log("Ainda não começou");
 // }, 3000);
 
 console.log("Ainda não começou 2");
+
+// 3 - Promise
+console.log(" ==== Promise ====");
+
+const promessa = Promise.resolve(5 + 5);
+
+console.log("Algum código!");
+
+promessa
+    .then((value) => {
+        console.log(`A soma é ${value}`);
+        return value;
+    })
+    .then((value) => value - 1)
+    .then((value) => console.log(`Agora é: ${value}`));
