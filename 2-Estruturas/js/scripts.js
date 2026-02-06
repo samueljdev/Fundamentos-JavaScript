faa8301f305377eb0dc91c6f9f9bfce016a03e6e
samuel <samuel.jdev11@gmail.com>
2026-02-06 09:03:49 -0400

    Mais sobre variáveis

--- 2-Estruturas/js/scripts.js
+++ 2-Estruturas/js/scripts.js
@@ -9,3 +9,23 @@ console.log(nome);
 
 const idade = 31;
 console.log(idade);
+
+// 2 - Mais sobre variáveis
+console.log("Mais sobre variáveis ==");
+
+// let 2teste = "inválido";
+// let @teste = "inválido";
+
+let a = 10,
+    b = 20,
+    c = 30;
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+const nomeCompleto = "João da Silva";
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+let _teste = "válido";
+let $teste = "válido";
+console.log(_teste, $teste);
