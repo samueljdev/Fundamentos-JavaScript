b4dd0e54df9801e406bb5c4f62009740f5cfdd11
samuel <samuel.jdev11@gmail.com>
2026-02-07 00:22:06 -0400

    Altura e largura

--- 8-Dom/js/scripts.js
+++ 8-Dom/js/scripts.js
@@ -86,3 +86,14 @@ firstLink.setAttribute("href", "https://www.google.com/");
 console.log(firstLink.getAttribute("href"));
 
 firstLink.setAttribute("target", "_blank");
+
+// 11 - Altura e largura
+console.log(" ==== Altura e largura ====");
+
+const footer = document.querySelector("footer");
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth);
+console.log(footer.clientHeight);
