7b43528d4195085b8dd557bc12c55af8228549e1
samuel <samuel.jdev11@gmail.com>
2026-02-10 18:49:09 -0400

    Requisição post

--- 11-Utilizando-axios/js/scripts.js
+++ 11-Utilizando-axios/js/scripts.js
@@ -46,3 +46,18 @@ const printData = async () => {
 };
 
 printData();
+
+// 5 - Requisição post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    axios.post("https://jsonplaceholder.typicode.com/posts", {
+        title: titleInput.value,
+        body: bodyInput.value,
+        userId: 1,
+    });
+});
