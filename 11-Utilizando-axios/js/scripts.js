ead9562ca600127110dfa60fdeaae12bc7d79211
samuel <samuel.jdev11@gmail.com>
2026-02-10 18:15:02 -0400

    Primeiro request

--- 11-Utilizando-axios/js/scripts.js
+++ 11-Utilizando-axios/js/scripts.js
@@ -1,2 +1,17 @@
 // 1 - instalando o axios
 console.log(axios);
+
+// 2 - Primeiro request
+// função async espera os dados chegarem
+const getData = async () => {
+    try {
+        // await espera a resposta do servidor
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.log(error.response);
+    }
+};
+
+getData();
